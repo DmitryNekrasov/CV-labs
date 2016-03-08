4e1d84644a0b4a0f7b33371d3b69a2b4f16ef494
DmitryNekrasov <scannorone@gmail.com>
2016-03-08 15:20:15 +0600

    Построение пирамиды с -1-й октавы

--- main.cpp
+++ main.cpp
@@ -7,17 +7,32 @@
 #include "myimage.h"
 #include "simple.h"
 
+static bool g_BuildMinusOneOctave = false;
+
+static const int min_size = 64;
+
 int main() {
 
     QImage qimage_in("/Users/ScanNorOne/Desktop/laferrari.jpg");
     mycv::CMyImage image_in(qimage_in);
 
-    int n = 3;
     int s = 3;
     double sigma_a = 0.5;
     double sigma_0 = 1.6;
+    auto n = int(std::min(std::log2(double(image_in.getHeight()) / min_size),
+                      std::log2(double(image_in.getWidth()) / min_size))) + 1;
 
-    auto gauss_pyramid = mycv::getGaussPyramid(image_in, n, s, sigma_a, sigma_0);
+    mycv::GaussPyramidT gauss_pyramid;
+
+    if (!g_BuildMinusOneOctave) {
+        gauss_pyramid = mycv::getGaussPyramid(image_in, n, s, sigma_a, sigma_0);
+    } else {
+        auto upscale = mycv::getUpscale(image_in);
+        gauss_pyramid = mycv::getGaussPyramid(upscale, n + 1, s, sigma_a, sigma_0);
+        for (auto& layer : gauss_pyramid) {
+            std::get<toUType(mycv::PyramidLayer::EffectiveSigma)>(layer) /= 2;
+        }
+    }
 
     int num = 0;
     for (const auto& layer : gauss_pyramid) {
@@ -34,10 +49,10 @@ int main() {
         img.toQImagePtr()->save(name.c_str());
     }
 
-    int y = 120;
-    int x = 350;
+    int y = 320;
+    int x = 305;
     auto intensity1 = mycv::getL(image_in, gauss_pyramid, y, x, 12.0);
-    auto intensity2 = mycv::getL(image_in, gauss_pyramid, y, x, 7.0);
+    auto intensity2 = mycv::getL(image_in, gauss_pyramid, y, x, 1.0);
 
     std::cout << intensity1 << " " << intensity2 << std::endl;
 
