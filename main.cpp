ce57a24243c6ed27525c6a46057c289d98694f39
DmitryNekrasov <scannorone@gmail.com>
2016-04-16 12:26:46 +0700

    Вывожу DoG

--- main.cpp
+++ main.cpp
@@ -10,59 +10,35 @@
 #include "matrix.h"
 #include "myimage.h"
 #include "poi.h"
+#include "pyramid.h"
 #include "qimagegenerator.h"
 #include "simple.h"
 
+static bool g_BuildMinusOneOctave = false;
+static const int g_MinSize = 16;
+
 int main() {
 
-    QImage first_qimage_in("/Users/ScanNorOne/Desktop/q1.png");
-    mycv::CMyImage first_image_in(first_qimage_in);
-    QImage second_qimage_in("/Users/ScanNorOne/Desktop/q2.png");
-    mycv::CMyImage second_image_in(second_qimage_in);
+    QImage qimage_in("/Users/ScanNorOne/Desktop/q.png");
+    mycv::CMyImage image_in(qimage_in);
 
-    int w_size = 3;
-    double threshold = 5.0;
-    int p_size = 5;
-    size_t target_quantity = 1000;
+    size_t s = 8;
+    double sigma_a = 0.5;
+    double sigma_0 = 1.6;
+    auto n = size_t(std::min(std::log2(double(image_in.getHeight()) / g_MinSize),
+                      std::log2(double(image_in.getWidth()) / g_MinSize))) + 1;
 
-    auto first_harris = mycv::poi::applyHarris(first_image_in, w_size);
-    auto first_poi = mycv::poi::getPoi(first_harris, threshold, p_size);
-    auto first_filtered_poi = mycv::poi::filterPoint(first_poi, target_quantity);
+    auto gauss_pyramid = mycv::getGaussPyramid(image_in, n, s, sigma_a, sigma_0, g_BuildMinusOneOctave);
 
-    auto second_harris = mycv::poi::applyHarris(second_image_in, w_size);
-    auto second_poi = mycv::poi::getPoi(second_harris, threshold, p_size);
-    auto second_filtered_poi = mycv::poi::filterPoint(second_poi, target_quantity);
+    int y = 20;
+    int x = 205;
+    auto intensity1 = mycv::getL(gauss_pyramid, y, x, 25.0);
+    auto intensity2 = mycv::getL(gauss_pyramid, y, x, 0.0);
 
-    std::cout << "First filtered point count: " << first_filtered_poi.size() << std::endl;
-    std::cout << "Second filtered point count: " << second_filtered_poi.size() << std::endl;
+    std::cout << intensity1 << " " << intensity2 << std::endl;
 
-    int descriptor_size = 4;
-    int block_size = 4;
-    int histogram_value_number = 8;
-
-    auto first_descriptors = mycv::desc::getDescriptors(first_image_in, first_filtered_poi,
-                                                        descriptor_size, block_size, histogram_value_number);
-    auto second_descriptors = mycv::desc::getDescriptors(second_image_in, second_filtered_poi,
-                                                        descriptor_size, block_size, histogram_value_number);
-
-    auto matches = mycv::desc::getMatches(std::get<0>(first_descriptors), std::get<0>(second_descriptors));
-
-    std::cout << matches.size() << std::endl;
-
-    first_harris.normalize();
-    second_harris.normalize();
-
-    mycv::qimg::toQImagePtr(first_harris)->save("/Users/ScanNorOne/Desktop/first_harris.png");
-    mycv::qimg::drawPoints(first_image_in, first_poi)->save("/Users/ScanNorOne/Desktop/first_poi.png");
-    mycv::qimg::drawPoints(first_image_in, first_filtered_poi)->save("/Users/ScanNorOne/Desktop/first_filtered_poi.png");
-
-    mycv::qimg::toQImagePtr(second_harris)->save("/Users/ScanNorOne/Desktop/second_harris.png");
-    mycv::qimg::drawPoints(second_image_in, second_poi)->save("/Users/ScanNorOne/Desktop/second_poi.png");
-    mycv::qimg::drawPoints(second_image_in, second_filtered_poi)->save("/Users/ScanNorOne/Desktop/second_filtered_poi.png");
-
-    mycv::qimg::showDescriptors(first_image_in, second_image_in, first_filtered_poi, second_filtered_poi,
-                                std::get<1>(first_descriptors), std::get<1>(second_descriptors), descriptor_size * block_size,
-                                matches, 255, 128)->save("/Users/ScanNorOne/Desktop/merged.png");
+    auto dog = mycv::getDog(gauss_pyramid);
+    mycv::savePyramid(dog, "/Users/ScanNorOne/Desktop/");
 
     return 0;
 }
